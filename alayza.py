ddbf76f5340f14b395e193989990278a31dc60ec
AndrejVysny <vysnyandrej@gmail.com>
2022-12-03 18:47:42 +0100

    changes

--- alayza.py
+++ alayza.py
@@ -3,19 +3,31 @@ import matplotlib.pyplot as plt
 
 
 def getPrechod(data):
-    return data
+
+    char = {}
+    i = 0
+    for index, row in data.iterrows():
+        if(index > 105 and index < 301):
+            try:
+                char[i].append(row['2'])
+            except:
+                char[i] = []
+                char[i].append(row['2'])
+            i = i + 1
+
+    return char
 
 
 def getPrevod(data):
 
     char = {}
     for index, row in data.iterrows():
+    
         try:
             char[int(row['1'])].append(row['2'])
         except:
-            char[int(row['1'])] = []
-            char[int(row['1'])].append(row['2'])
-
+             char[int(row['1'])] = []
+             char[int(row['1'])].append(row['2'])
 
     res = {}
 
@@ -37,29 +49,27 @@ versions = [
 
 
 pos = 0
+
 fig, axs = plt.subplots(4,2)
 
-
-
 for name in versions:
-    
+
     data = pd.read_csv(name)
 
-    df = pd.DataFrame(data)
-    char = {}
-    for index, row in df.iterrows():
-        
-        char[row['1']] =  row['2']
-    
     axs[0,pos].scatter(data.index, data['1'], color='red', label='Input',zorder=5, s=1)
     axs[0,pos].plot(data.index, data['2'], color='blue', label='Output',zorder=0, lw=1, alpha=0.5)
     axs[0,pos].plot(data.index, data['3'], color='aqua', label='Load',zorder=0, lw=1, alpha=0.5)
     axs[0,pos].legend()
+    axs[0, pos].set_title('Vstupy')
     axs[0,pos].set_xlabel("Time [ms]")
     axs[0, pos].set_ylabel("Napätie [V]")
-
-
+    
     trans = getPrevod(data)
+
+    if(name == 'vystup4'):
+        trans = getPrevod(data[80:100])
+
+# TODO: if vystup4 get data from index 80 due to load input error
     axs[1, pos].scatter(trans.keys(),trans.values())
     axs[1, pos].plot(trans.keys(),trans.values())
     axs[1, pos].set_title('Prevodová charakteristika')
@@ -70,9 +80,40 @@ for name in versions:
 
 
 # Okolie bodu 3
-axs[2, 0].plot(range(1,35),range(1,35))
-axs[2, 1].plot(range(1,40),range(1,40))
+
+data = pd.read_csv("PB3_ST0_7.csv")
+prechod = getPrechod(data)
+axs[2, 0].plot(prechod.keys(),prechod.values())
+axs[2, 0].set_title('PB 3 Prechodová charakteristika')
+axs[2, 0].set_ylabel("Napätie [V]")
+axs[2, 0].set_xlabel("Step")
+
+data = pd.read_csv("PB3_ST0_7_z1.csv")
+prechod = getPrechod(data)
+axs[2, 1].plot(prechod.keys(),prechod.values())
+axs[2, 1].set_title('PB 3 Prechodová charakteristika so záťažou')
+axs[2, 1].set_ylabel("Napätie [V]")
+axs[2, 1].set_xlabel("Step")
+
 
 # Okolie bodu 5
+data = pd.read_csv("PB5_ST0_7.csv")
+prechod = getPrechod(data)
+axs[3, 0].plot(prechod.keys(),prechod.values())
+axs[3, 0].set_title('PB 5 Prechodová charakteristika')
+axs[3, 0].set_ylabel("Napätie [V]")
+axs[3, 0].set_xlabel("Step")
+
+data = pd.read_csv("PB5_ST0_7_z1.csv")
+prechod = getPrechod(data)
+axs[3, 1].plot(prechod.keys(),prechod.values())
+axs[3, 1].set_title('PB 5 Prechodová charakteristika so záťažou')
+axs[3, 1].set_ylabel("Napätie [V]")
+axs[3, 1].set_xlabel("Step")
+
+
+fig.set_size_inches(20, 20)
+# when saving, specify the DPI
+plt.savefig("myplot.png", dpi = 100)
 
 plt.show()
--- convert.py
+++ convert.py
@@ -4,6 +4,5 @@ import pandas as pd
 name = 'vystup4'
 mat = scipy.io.loadmat( name + '.mat')
 
-
 df = pd.DataFrame(mat[name])
 df.to_csv(name + '.csv')
